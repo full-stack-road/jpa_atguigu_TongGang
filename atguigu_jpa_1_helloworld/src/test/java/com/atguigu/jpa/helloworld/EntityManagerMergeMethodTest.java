d95717247d73b4cf964d9d6489183eb6c260fa53
cyhbyw <swust2009cyh@qq.com>
2017-11-30 16:55:10 +0800

    完善对EntityManager#Merge方法调用时，底层执行Sql语句的注释说明

--- atguigu_jpa_1_helloworld/src/test/java/com/atguigu/jpa/helloworld/EntityManagerMergeMethodTest.java
+++ atguigu_jpa_1_helloworld/src/test/java/com/atguigu/jpa/helloworld/EntityManagerMergeMethodTest.java
@@ -40,8 +40,8 @@ public class EntityManagerMergeMethodTest {
      * 若传入的是一个临时对象，则步骤如下：
      * 1. 创建一个新的对象
      * 2. 把临时对象的属性复制到新的对象中
-     * 3. 对新的对象执行持久化操作.
-     * 所以新的对象中有 id, 但以前的临时对象中没有 id.
+     * 3. 对新的对象执行 Insert 操作.
+     * 所以新的对象中有 id, 但以前的临时对象中没有 id. 同时，整个流程中不会有 Select 语句产生
      */
     @Test
     public void testMerge1() {
@@ -61,7 +61,7 @@ public class EntityManagerMergeMethodTest {
     /**
      * 若传入的是一个游离对象, 即传入的对象有 OID，则步骤如下：
      * 1. 若在 EntityManager 缓存中没有该对象
-     * 2. 若在数据库中也没有对应的记录
+     * 2. 若在数据库中也没有对应的记录（会有 Select 语句产生）
      * 3. JPA 会创建一个新的对象, 然后把当前游离对象的属性复制到新创建的对象中
      * 4. 对新创建的对象执行 insert 操作.
      */
@@ -85,7 +85,7 @@ public class EntityManagerMergeMethodTest {
     /**
      * 若传入的是一个游离对象, 即传入的对象有 OID. 步骤如下：
      * 1. 若在 EntityManager 缓存中没有该对象
-     * 2. 若在数据库中有对应的记录
+     * 2. 若在数据库中有对应的记录（会有 Select 语句产生）
      * 3. JPA 会查询对应的记录, 然后返回该记录对一个的对象, 再然后会把游离对象的属性复制到查询到的对象中.
      * 4. 对查询到的对象执行 update 操作.
      */
@@ -98,7 +98,7 @@ public class EntityManagerMergeMethodTest {
         customer.setEmail("ee@163.com");
         customer.setLastName("EE");
 
-        customer.setId(4);
+        customer.setId(1);
 
         Customer customer2 = entityManager.merge(customer);
 
@@ -110,7 +110,7 @@ public class EntityManagerMergeMethodTest {
      * 若传入的是一个游离对象, 即传入的对象有 OID.
      * 1. 若在 EntityManager 缓存中有对应的对象
      * 2. JPA 会把游离对象的属性复制到查询到 EntityManager 缓存的对象中.
-     * 3. EntityManager 缓存中的对象执行 UPDATE.
+     * 3. EntityManager 缓存中的对象执行 Update 操作.
      */
     @Test
     public void testMerge4() {
@@ -121,10 +121,10 @@ public class EntityManagerMergeMethodTest {
         customer.setEmail("ff@163.com");
         customer.setLastName("FF");
 
-        customer.setId(4);
+        customer.setId(1);
         System.out.println("customer: " + customer);
 
-        Customer customer2 = entityManager.find(Customer.class, 4);
+        Customer customer2 = entityManager.find(Customer.class, 1);
         //notice: birth=2017-11-16
         System.out.println("customer2: " + customer2);
 
